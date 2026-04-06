2611251c0d2ac06376c66faad22efa4a7acc2446
akshatsaxena26 <akshatsaxena023@gmail.com>
2026-04-06 10:18:33 +0530

    refactor: clean up comments and improve database connection log

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,21 @@
-//! create an express server and check if it's working
+
 
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose"; // cross origin resource sharing (browser blocks the request which comes from anywhere but localhost:8000)
-// 1) we are importing express module which we installed using npm i
+import mongoose from "mongoose"; 
 
 import userRoutes from "./routes/auth-route.js";
 
 
 mongoose.connect("mongodb://127.0.0.1:27017/project2")
 .then(() => {
-  console.log("Database connected ✅");
+  console.log("Database connected successfully");
 })
 .catch((err) => {
-  console.log("DB Error ❌", err);
+  console.log("DB Error ", err);
 });
 
-// 2) call/invoke the function
-let app = express(); // object = {listen}
+let app = express(); 
 
 app.use(
   cors({
@@ -28,19 +26,10 @@ app.use(
 app.use(express.urlencoded({ extended: true }));// this 
 app.use(express.json());
 
-app.use("/api/auth", userRoutes); // http://localhost:9001/api/auth/signup
+app.use("/api/auth", userRoutes); 
 
-// 3) assign a port number to our server
+
 app.listen(9001, () => {
   console.log("Server Started.....");
 });
-// app.listen(PORT_NUMBER, callback)
 
-//! to check if the server is running, in cmd(git bash), goto backend folder and type "npx nodemon index.js"
-// open browser -> localhost:PORT_NUMBER and press enter
-
-// https://nodejs.org/en/ (/) =>  this is base url
-// https://nodejs.org/en/blog => /blog is one endpoint
-// https://nodejs.org/en/download
-
-// https://github.com/Sarvesh-1999/NIGHT-CODING-MARATHON
